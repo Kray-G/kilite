49bbc3dd3df9743b46c289ae7f7d68f6bbf7d296
Kray-G <nagai.kazuya@gmail.com>
2022-10-28 10:52:24 +0900

    updated make.sh

--- build/make.sh
+++ build/make.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 CUR=$PWD
-cd `basename $0`
+cd `dirname $0`
+echo $PWD
 cd ..
 
 mkdir -p bin/mir
@@ -10,9 +11,20 @@ cmake ../../mir
 make
 cp -f c2m ../
 cp -f libmir_static.a ../
-
 cd ..
-gcc -O2 -o kilite \
+
+TEMPF=h.c
+
+# Create a header source code.
+gcc -O2 -o makecstr ../utils/makecstr.c
+cat ../src/template/lib/bign.h > $TEMPF
+cat ../src/template/lib/bigz.h >> $TEMPF
+cat ../src/template/header.h >> $TEMPF
+./makecstr $TEMPF > ../src/backend/header.c
+rm $TEMPF
+
+gcc -O2 -I ../mir \
+    -DUSE_INT64 -o kilite \
     ../src/main.c \
     ../src/frontend/lexer.c \
     ../src/frontend/parser.c \
@@ -20,6 +32,37 @@ gcc -O2 -o kilite \
     ../src/frontend/dispast.c \
     ../src/frontend/mkkir.c \
     ../src/backend/dispkir.c \
-    ../src/backend/translate.c
+    ../src/backend/translate.c \
+    ../src/backend/resolver.c \
+    ../src/backend/header.c \
+    ../src/backend/cexec.c \
+    ../bin/libmir_static.a
+
+cp -f kilite ../kilite
+
+mkdir -p lib
+cp -f ../src/template/lib/bign.h ./lib/bign.h
+cp -f ../src/template/lib/bigz.h ./lib/bigz.h
+cp -f ../src/template/common.h ./common.h
+cp -f ../src/template/header.h ./header.h
+cat ../src/template/lib/bign.c > $TEMPF
+cat ../src/template/lib/bigz.c >> $TEMPF
+cat ../src/template/alloc.c >> $TEMPF
+cat ../src/template/gc.c >> $TEMPF
+cat ../src/template/init.c >> $TEMPF
+cat ../src/template/main.c >> $TEMPF
+cat ../src/template/util.c >> $TEMPF
+cat ../src/template/bigi.c >> $TEMPF
+cat ../src/template/str.c >> $TEMPF
+cat ../src/template/hash.c >> $TEMPF
+./c2m -DUSE_INT64 -I lib -c $TEMPF
+rm kilite.bmir
+mv h.bmir kilite.bmir
+cp -f kilite.bmir ../kilite.bmir
+rm $TEMPF
+
+rm -f *.h
+rm -f makecstr
+rm -rf lib
 
 cd $CUR
