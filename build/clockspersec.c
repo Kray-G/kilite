7ffae9add616bb2d95471eedaeb1d8f27ebafc49
Kray-G <nagai.kazuya@gmail.com>
2022-12-05 22:47:28 +0900

    updated CLOCKS_PER_SEC.

--- build/clockspersec.c
+++ build/clockspersec.c
@@ -5,7 +5,7 @@ int main(int ac, char **av)
 {
     printf("#ifdef __MIRC__\n");
     printf("#ifndef CLOCKS_PER_SEC\n");
-    printf("#define CLOCKS_PER_SEC ((clock_t)%d)\n", CLOCKS_PER_SEC);
+    printf("#define CLOCKS_PER_SEC ((clock_t)%d)\n", (int)CLOCKS_PER_SEC);
     printf("#endif\n");
     printf("#endif\n");
     return 0;
