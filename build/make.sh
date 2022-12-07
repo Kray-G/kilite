dfa51878d2db37c27c0b6c789f91c5564790a41f
Kray-G <nagai.kazuya@gmail.com>
2022-12-07 22:47:11 +0900

    fixed a buld script.

--- build/make.sh
+++ build/make.sh
@@ -52,53 +52,53 @@ echo "#define KILITE_AMALGAMATION" > $TEMPF
 ./clockspersec >> $TEMPF
 cat ../src/template/lib/bign.h >> $TEMPF
 cat ../src/template/lib/bigz.h >> $TEMPF
-echo "#ifdef __MIRC__" >> %TEMPF%
+echo "#ifdef __MIRC__" >> $TEMPF
 cat ../src/template/lib/printf.h >> $TEMPF
-echo "#endif" >> %TEMPF%
+echo "#endif" >> $TEMPF
 cat ../src/template/header.h >> $TEMPF
 cat ../src/template/lib/bign.c >> $TEMPF
 cat ../src/template/lib/bigz.c >> $TEMPF
-echo "#ifdef __MIRC__" >> %TEMPF%
+echo "#ifdef __MIRC__" >> $TEMPF
 cat ../src/template/lib/printf.c >> $TEMPF
-echo "#endif" >> %TEMPF%
-echo "#line 1 \"alloc.c\"" >> %TEMPF%
+echo "#endif" >> $TEMPF
+echo "#line 1 \"alloc.c\"" >> $TEMPF
 cat ../src/template/alloc.c >> $TEMPF
-echo "#line 1 \"gc.c\"" >> %TEMPF%
+echo "#line 1 \"gc.c\"" >> $TEMPF
 cat ../src/template/gc.c >> $TEMPF
-echo "#line 1 \"init.c\"" >> %TEMPF%
+echo "#line 1 \"init.c\"" >> $TEMPF
 cat ../src/template/init.c >> $TEMPF
-echo "#line 1 \"main.c\"" >> %TEMPF%
+echo "#line 1 \"main.c\"" >> $TEMPF
 cat ../src/template/main.c >> $TEMPF
-echo "#line 1 \"util.c\"" >> %TEMPF%
+echo "#line 1 \"util.c\"" >> $TEMPF
 cat ../src/template/util.c >> $TEMPF
-echo "#line 1 \"format.c\"" >> %TEMPF%
+echo "#line 1 \"format.c\"" >> $TEMPF
 cat ../src/template/format.c >> $TEMPF
-echo "#line 1 \"bigi.c\"" >> %TEMPF%
+echo "#line 1 \"bigi.c\"" >> $TEMPF
 cat ../src/template/bigi.c >> $TEMPF
-echo "#line 1 \"str.c\"" >> %TEMPF%
+echo "#line 1 \"str.c\"" >> $TEMPF
 cat ../src/template/str.c >> $TEMPF
-echo "#line 1 \"obj.c\"" >> %TEMPF%
+echo "#line 1 \"obj.c\"" >> $TEMPF
 cat ../src/template/obj.c >> $TEMPF
-echo "#line 1 \"op.c\"" >> %TEMPF%
+echo "#line 1 \"op.c\"" >> $TEMPF
 cat ../src/template/op.c >> $TEMPF
-echo "#line 1 \"lib.h\"" >> %TEMPF%
+echo "#line 1 \"lib.h\"" >> $TEMPF
 cat ../src/template/lib.h >> $TEMPF
-echo "#line 1 \"libstd.c\"" >> %TEMPF%
+echo "#line 1 \"libstd.c\"" >> $TEMPF
 cat ../src/template/libstd.c >> $TEMPF
-echo "#line 1 \"libxml.c\"" >> %TEMPF%
+echo "#line 1 \"libxml.c\"" >> $TEMPF
 cat ../src/template/libxml.c >> $TEMPF
-echo "#line 1 \"inc/platform.c\"" >> %TEMPF%
+echo "#line 1 \"inc/platform.c\"" >> $TEMPF
 cat ../src/template/inc/platform.c >> $TEMPF
-echo "#ifndef __MIRC__" >> %TEMPF%
-echo "#line 1 \"miniz.h\"" >> %TEMPF%
-cat ../src/template/lib/miniz.h >> %TEMPF%
-echo "#line 1 \"zip.h\"" >> %TEMPF%
-cat ../src/template/lib/zip.h >> %TEMPF%
-echo "#line 1 \"zip.c\"" >> %TEMPF%
-cat ../src/template/lib/zip.c >> %TEMPF%
-echo "#endif" >> %TEMPF%
-echo "#line 1 \"libtzip.c\"" >> %TEMPF%
-cat ../src/template/libtzip.c >> %TEMPF%
+echo "#ifndef __MIRC__" >> $TEMPF
+echo "#line 1 \"miniz.h\"" >> $TEMPF
+cat ../src/template/lib/miniz.h >> $TEMPF
+echo "#line 1 \"zip.h\"" >> $TEMPF
+cat ../src/template/lib/zip.h >> $TEMPF
+echo "#line 1 \"zip.c\"" >> $TEMPF
+cat ../src/template/lib/zip.c >> $TEMPF
+echo "#endif" >> $TEMPF
+echo "#line 1 \"libtzip.c\"" >> $TEMPF
+cat ../src/template/libtzip.c >> $TEMPF
 cd ../src/template/std
 $BIN/kilite --makelib callbacks.klt >> $TEMPF
 cd $BIN
